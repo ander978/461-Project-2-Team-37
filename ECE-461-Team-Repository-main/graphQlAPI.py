2443321e26f2a9db3933e717c0336d57c4a72228
David20736 <davidxin1024@gmail.com>
2023-04-29 16:36:40 -0400

    this should fix linter errors on graphQlAPI

--- ECE-461-Team-Repository-main/graphQlAPI.py
+++ ECE-461-Team-Repository-main/graphQlAPI.py
@@ -1,5 +1,7 @@
-import requests
 from urllib.parse import urlparse
+
+import requests
+
 # import re
 # import urlParser
 # from decouple import config
@@ -12,10 +14,15 @@ def generateQuery(url):
     owner = '"' + repoDetails[1] + '"'
     name = '"' + repoDetails[2] + '"'
 
-    graphQlQuery = """
+    graphQlQuery = (
+        """
     query {
-      repositoryOwner (login: """ + owner + """) {
-        repository(name: """ + name + """) {
+      repositoryOwner (login: """
+        + owner
+        + """) {
+        repository (name: """
+        + name +
+        """) {
           forks {
             totalCount
           }
@@ -53,30 +60,31 @@ def generateQuery(url):
         }
       }
     }"""
+    )
     # print(graphQlQuery)
     return graphQlQuery
 
 
 # A simple function to use requests.post to make the API call. Note the json= section.
-def run_graphQlQuery(graphQlQuery, token):
+def run_graphQlQuery(graphQlQuery, token, logPath):
     headers = {"Authorization": "Bearer " + token}
     request = requests.post(
-        'https://api.github.com/graphql', json={'query': graphQlQuery}, headers=headers
+        'https://api.github.com/graphql', json={"query": graphQlQuery}, headers=headers
     )
     if request.status_code == 200:
         return request.json()
     else:
+        with open(logPath, "a") as f:
+            print(
+                "Query failed to run by returning code of {}. {}".format(
+                    request.status_code, graphQlQuery
+                ),
+                file=f
+            )
         raise Exception(
             "Query failed to run by returning code of {}. {}".format(
                 request.status_code, graphQlQuery
             )
-        )
-        with open(logFilePath, "a") as f:
-            print(
-                "Query failed to run by returning code of {}. {}".format(
-                    request.status_code, graphQlQuery
-            ),
-            file=f
         )
 
 
@@ -84,7 +92,7 @@ def generateGraphQLData(validUrls, token, logFilePath):
     graphQlData = []
     for url in validUrls:
         query = generateQuery(url)
-        result = run_graphQlQuery(query, token)
+        result = run_graphQlQuery(query, token, logFilePath)
         graphQlData.append(result["data"]["repositoryOwner"]["repository"])
     # for i in graphQlData: print("Info - {}".format(i))
     with open("outputGraphQl.txt", "w") as f:
